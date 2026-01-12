118f91b4e7b089d9aa9f1072cfaa44a5afa0ee58
Daniel Mendez <danielmendez1708@hotmail.com>
2026-01-11 19:31:57 -0700

    Configuring postgreSQL as the default database engine

--- store/models.py
+++ store/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
