b47e29407d206a2c96ed4bc177d53f6f809312c7
Daniel Mendez <danielmendez1708@hotmail.com>
2026-01-25 13:12:53 -0700

    Extending User model to add email

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
 from store.models import Product
 
+from .models import User
 # We need to add the app to settings.py
+
+# Registering admin model to managing our users
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
 
 # Using Generic Relationships
 # Class for managing Tags
--- core/models.py
+++ core/models.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+# Changin store custom to core. This is the core of our proyect, where all the apps will comunicate
+# Extending user
+# If you do this in middle of a proyect you will have errors with migrations, and will need to restart the database, so it's a best practice to start this class with pass at the begining of a proyect, despite you will not use it
+class User(AbstractUser):
+    # Adding a new field to the user model, to enable login with email
+    email = models.EmailField(unique=True)
