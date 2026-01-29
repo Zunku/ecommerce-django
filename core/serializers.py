dcccac556648186fbf99d537c47c9b70d62e6315
Daniel Mendez <danielmendez1708@hotmail.com>
2026-01-29 11:35:15 -0700

    Creating custom serializer UserSerializer to show first and last name of current user

--- core/serializers.py
+++ core/serializers.py
@@ -1,14 +1,20 @@
 # Importing serializer for UserCreation
-from djoser.serializers import UserCreateSerializer as BaseUCS
+from djoser.serializers import UserCreateSerializer as BaseUCS, UserSerializer as BaseUS
 from rest_framework import serializers
+from core.models import User
 
 # Creating a custom serializer for registering users
 class UserCreateSerializer(BaseUCS):
+    # We can access birth_date because we have a relationship with user in the Customer model
     birth_date = serializers.DateField()
     
     # Always is better to inherit all from a class and overwrite only the fields you need
     class Meta(BaseUCS.Meta):
         # Adding extra fields to the serializer
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'birth_date']
         
-    
+# Custom serializer to show first and last name of current user
+class UserSerializer(BaseUS):
+    class Meta(BaseUS.Meta):
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
