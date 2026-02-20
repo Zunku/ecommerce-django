8059bd1b6ee72343877b69d860d8d4ce125e1411
Daniel Mendez <danielmendez1708@hotmail.com>
2026-02-19 18:29:56 -0700

    Creating a test model

--- store/models.py
+++ store/models.py
@@ -134,6 +134,3 @@ class Review(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='review')
-    
-class Pepitoria(models.Model):
-    title = models.CharField(max_length=255)
